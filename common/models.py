8d0b514e90712eaad427ea8525785e1772f1f134
Kulbear <jiyang.py@gmail.com>
2018-10-14 14:03:22 -0600

    Merge two similar implementations of the scSE block

--- common/models.py
+++ common/models.py
@@ -58,8 +58,9 @@ class HengDecoder(nn.Module):
 
 
 class SCSEBlock(nn.Module):
-    def __init__(self, channel, reduction=16):
+    def __init__(self, channel, reduction=16, ordinary=True):
         super(SCSEBlock, self).__init__()
+        self.ordinary = ordinary
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
 
         self.channel_excitation = nn.Sequential(nn.Linear(channel, int(channel // reduction)),
@@ -81,32 +82,10 @@ class SCSEBlock(nn.Module):
 
         spa_se = self.spatial_se(x)
         spa_se = torch.mul(x, spa_se)
-        return chn_se + spa_se
-
-
-class ModifiedSCSEBlock(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(ModifiedSCSEBlock, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-
-        self.channel_excitation = nn.Sequential(nn.Linear(channel, int(channel // reduction)),
-                                                nn.ReLU(inplace=True),
-                                                nn.Linear(int(channel // reduction), channel),
-                                                nn.Sigmoid())
-
-        self.spatial_se = nn.Sequential(nn.Conv2d(channel, 1, kernel_size=1,
-                                                  stride=1, padding=0, bias=False),
-                                        nn.Sigmoid())
-
-    def forward(self, x):
-        bahs, chs, _, _ = x.size()
-
-        # Returns a new tensor with the same data as the self tensor but of a different size.
-        chn_se = self.avg_pool(x).view(bahs, chs)
-        chn_se = self.channel_excitation(chn_se).view(bahs, chs, 1, 1)
-
-        spa_se = self.spatial_se(x)
-        return torch.mul(torch.mul(x, chn_se), spa_se)
+        if self.ordinary:
+            return chn_se + spa_se
+        else:
+            return torch.mul(torch.mul(x, chn_se), spa_se)
 
 
 class UNetResNet(nn.Module):
